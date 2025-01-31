1b877380e7723cc69c813a34efcccf42b1b5e137
Micael Jerry Fidimalala <mfidimal@student.42antananarivo.mg>
2025-01-31 16:37:36 +0300

    chore: char to binary rep string

--- src/client/crypt.c
+++ src/client/crypt.c
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   crypt.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/29 19:49:58 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/31 16:28:52 by mfidimal         ###   ########.fr       */
+/*   Created: 2025/01/31 15:49:53 by mfidimal          #+#    #+#             */
+/*   Updated: 2025/01/31 16:30:24 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-#define MINITALK_H
+#include "minitalk.h"
 
-#include "../ft_printf/include/ft_printf.h"
-#include <sys/types.h>
-#include <signal.h>
+char *char_to_binary(unsigned char c)
+{
+	char *res;
+	int i;
 
-// client
-char	*char_to_binary(unsigned char c);
-
-// server
-void	sig_handler(int sig_num);
-
-void	put_error(char *err);
-
-#endif
+	res = ft_strdup("00000000");
+	i = 0;
+	while (i < 8)
+	{
+		if (c & (1 << i))
+			res[7 - i] = '1';
+		i++;
+	}
+	return (res);
+}
