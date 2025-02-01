2c0e058995327810922b29600dd575ad0efa22da
Micael Jerry Fidimalala <mfidimal@student.42antananarivo.mg>
2025-02-01 09:48:20 +0300

    fix: redirect error on error output

--- src/client/client.c
+++ src/client/client.c
@@ -6,11 +6,24 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 08:31:44 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/02/01 06:33:16 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/02/01 09:47:20 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+static void send_one_signal(int pid, int sig_val)
+{
+	int response;
+
+	response = kill(pid, sig_val);
+
+	if (response == -1)
+	{
+		ft_putendl_fd("Error while sending signal (invalid PID, ...)", 2);
+		exit(1);
+	}
+}
 
 static void	send_signal(int pid, char *msg)
 {
@@ -24,9 +37,9 @@ static void	send_signal(int pid, char *msg)
 		while (j < 8)
 		{
 			if (msg[i] >> (7 - j) & 1)
-				kill(pid, SIGUSR2);
+				send_one_signal(pid, SIGUSR2);
 			else
-				kill(pid, SIGUSR1);
+				send_one_signal(pid, SIGUSR1);
 			usleep(500);
 			j++;
 		}
@@ -46,12 +59,12 @@ int	main(int argc, const char *argv[])
 	int	pid;
 
 	if (argc != 3)
-		return (ft_printf("Invalid Args\n"), 1);
+		return (ft_putendl_fd("Invalid Args", 2), 1);
 	pid = ft_atoi((char *)argv[1]);
 	if (pid <= 0)
-		return (ft_printf("Invalid PID\n"), 1);
+		return (ft_putendl_fd("Invalid PID", 2), 1);
 	if (!argv[2][0])
-		return (ft_printf("Empty message\n"), 1);
+		return (ft_putendl_fd("Empty message", 2), 1);
 	send_signal(pid, (char *)argv[2]);
 	return (0);
 }
--- src/server/error.c
+++ src/server/error.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 15:10:51 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/31 15:12:24 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/02/01 09:43:28 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	put_error(char *err)
 {
-	ft_printf("%s\n", err);
-	exit(0);
+	ft_putstr_fd(err, 2);
+	exit(1);
 }
